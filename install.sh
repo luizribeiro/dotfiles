8a8e40ae6b13ff7ce585221024d03a2a15242b74
Luiz Ribeiro <luizribeiro@gmail.com>
2014-10-03 23:03:26 -0700

    Made install.sh slightly more readable

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ pushd `dirname $0` > /dev/null
 DOTFILES_PATH=`pwd`
 popd > /dev/null
 
-ln -s $DOTFILES_PATH/.gitconfig ~/.gitconfig
-ln -s $DOTFILES_PATH/.zshrc ~/.zshrc
-ln -s $DOTFILES_PATH/.tmux.conf ~/.tmux.conf
+DOTFILES=(
+  .gitconfig
+  .tmux.conf
+  .zshrc
+)
+
+for dotfile in "${DOTFILES[@]}"
+do
+  ln -s $DOTFILES_PATH/$dotfile ~/$dotfile
+done
