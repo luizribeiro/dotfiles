a6453a2ef8000a4703fc163113e5652dd19b8d5f
Luiz Ribeiro <luizribeiro@gmail.com>
2020-06-18 13:18:54 -0400

    [osx-setup] Fixed SecurityAndPrivacy

--- osx-setup.sh
+++ osx-setup.sh
@@ -91,7 +91,7 @@ SecurityAndPrivacy() {
   # require password immediately after sleep or screen saver begins
   defaults write com.apple.screensaver askForPassword -int 1
   defaults write com.apple.screensaver askForPasswordDelay -int 0
-}
+} ; setup SecurityAndPrivacy
 
 if [[ ! ($* == *--no-restart*) ]]; then
   echo ""
