0d2e7e295e077cc860e8a8da1f32c9da60fbbf5b
Luiz Ribeiro <luizribeiro@gmail.com>
2020-06-18 16:02:07 -0400

    [osx] Enable tap to click on trackpad

--- osx-setup.sh
+++ osx-setup.sh
@@ -63,6 +63,11 @@ Keyboard() {
 } ; setup Keyboard
 
 Trackpad() {
+  # Enable tap to click (Trackpad) for this user and for the login screen
+  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+  defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+  defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
   # map bottom right corner to right-click
   defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
   defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
